f648eefc2c0c41193cceb38f23952f62562e252a
Xyedo <hafidmahdi23@gmail.com>
2022-11-02 00:08:53 +0700

    add production code

--- dethumanvscow.py
+++ dethumanvscow.py
@@ -41,11 +41,12 @@ def detect():
     # Run inference
     if device.type != 'cpu':
         model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
-
+    old_img_w = old_img_h = imgsz
+    old_img_b = 1
     t0 = time.time()
     cap = cv2.VideoCapture(source)
     cap.set(cv2.CAP_PROP_BUFFERSIZE, 3)
-    warmup = True
+
     while cap.isOpened():
         ret, im0 = cap.read()
         # im0 = cv2.cvtColor(im0, cv2.COLOR_GRAY2RGB)
@@ -60,10 +61,13 @@ def detect():
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
 
         # Warmup
-        if warmup is True and device.type != 'cpu':
+        if device.type != 'cpu' and (
+                old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
+            old_img_b = img.shape[0]
+            old_img_h = img.shape[2]
+            old_img_w = img.shape[3]
             for i in range(3):
                 model(img, augment=False)[0]
-            warmup = False
         if img.ndimension() == 3:
             img = img.unsqueeze(0)
         # Inference
@@ -78,7 +82,7 @@ def detect():
 
         # Process detections
         for i, det in enumerate(pred):  # detections per image
-            s = '%g: ' % i
+            s = ''
             if len(det) == 0:
                 continue
             # Rescale boxes from img_size to im0 size
@@ -90,8 +94,7 @@ def detect():
 
             # Write results
             for *xyxy, conf, cls in reversed(det):
-                if names[int(cls)] != "Human":
-                    continue
+
                 label = f'{names[int(cls)]} {conf:.2f}'
                 plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
 
@@ -113,7 +116,7 @@ def detect():
             # --ENDED
             # Print time (inference + NMS)
             print(f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}s) NMS')
-            cv2.imshow(str("humanvscow"), im0)
+            cv2.imshow("humanvscow", im0)
             cv2.waitKey(1)  # 1 millisecond
 
     print(f'Done. ({time.time() - t0:.3f}s)')
