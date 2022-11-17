60b9e9f38b6d8d8948ebcd788ef0c60860ecc2e3
xyedo <hafidmahdi23@gmail.com>
2022-11-17 20:17:13 +0700

    prod run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@ activate() {
 	source "/home/xyedo/project/python/python-venv/yolov7/bin/activate"
 }
 activate
-python /home/xyedo/project/python/HumanVsCow-yolov7/dethumanvscow.py --weights best.pt --source "v4l2src device=/dev/video0 ! video/x-raw, format= GRAY8 ! videoconvert ! appsink max-buffers=1 drop=true " --alarm --connect-rtdb
+cd "/home/xyedo/project/python/HumanVsCow-yolov7"
+
+python ./dethumanvscow.py --weights ./best.pt --source "v4l2src device=/dev/video0 ! video/x-raw, format= GRAY8 ! videoconvert ! appsink max-buffers=1 drop=true " --alarm --connect-rtdb
