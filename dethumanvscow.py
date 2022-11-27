eac10837be5913ff9fc98808a256f3247eff9ce2
Xyedo <hafidmahdi23@gmail.com>
2022-11-27 14:04:59 +0700

    feat:add clean handler keyboardintterupt

--- dethumanvscow.py
+++ dethumanvscow.py
@@ -1,5 +1,5 @@
 import argparse
-import time
+import sys
 import threading
 import cv2
 import torch
@@ -33,87 +33,90 @@ def detect():
     cudnn.benchmark = True  # set True to speed up constant image size inference
     dataset = LoadWebcam(source, img_size=imgsz, stride=stride)
 
-    # Get names and colors
-    names = model.module.names if hasattr(model, 'module') else model.names
-    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+    try:
+        # Get names and colors
+        names = model.module.names if hasattr(model, 'module') else model.names
+        colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
 
-    # Run inference
-    if device.type != 'cpu':
-        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
-    old_img_w = old_img_h = imgsz
-    old_img_b = 1
+        # Run inference
+        if device.type != 'cpu':
+            model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+        old_img_w = old_img_h = imgsz
+        old_img_b = 1
 
-    t0 = time.time()
-    for path, img, im0s, vid_cap in dataset:
-        img = torch.from_numpy(img).to(device)
-        img = img.half() if half else img.float()  # uint8 to fp16/32
-        img /= 255.0  # 0 - 255 to 0.0 - 1.0
-        if img.ndimension() == 3:
-            img = img.unsqueeze(0)
+        for path, img, im0s, vid_cap in dataset:
+            img = torch.from_numpy(img).to(device)
+            img = img.half() if half else img.float()  # uint8 to fp16/32
+            img /= 255.0  # 0 - 255 to 0.0 - 1.0
+            if img.ndimension() == 3:
+                img = img.unsqueeze(0)
 
-        # Warmup
-        if device.type != 'cpu' and (
-                old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
-            old_img_b = img.shape[0]
-            old_img_h = img.shape[2]
-            old_img_w = img.shape[3]
-            for i in range(3):
-                model(img, augment=True)[0]
+            # Warmup
+            if device.type != 'cpu' and (
+                    old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
+                old_img_b = img.shape[0]
+                old_img_h = img.shape[2]
+                old_img_w = img.shape[3]
+                for i in range(3):
+                    model(img, augment=True)[0]
 
-        # Inference
-        t1 = time_synchronized()
-        with torch.no_grad():  # Calculating gradients would cause a GPU memory leak
-            pred = model(img, augment=True)[0]
-        t2 = time_synchronized()
-        if opt.connect_rtdb:
-            chk_alarm = threading.Thread(target=realtime.check_alarm, args=())
-            chk_alarm.start()
-        # Apply NMS
-        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, agnostic=True)
-        t3 = time_synchronized()
+            # Inference
+            t1 = time_synchronized()
+            with torch.no_grad():  # Calculating gradients would cause a GPU memory leak
+                pred = model(img, augment=True)[0]
+            t2 = time_synchronized()
+            if opt.connect_rtdb:
+                chk_alarm = threading.Thread(target=realtime.check_alarm, args=())
+                chk_alarm.start()
+            # Apply NMS
+            pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, agnostic=True)
+            t3 = time_synchronized()
 
-        # Process detections
-        for i, det in enumerate(pred):  # detections per image
-            p, s, im0 = path, '', im0s
-            if len(det) == 0:
-                continue
-            # Rescale boxes from img_size to im0 size
-            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
-            # Print results
-            for c in det[:, -1].unique():
-                n = (det[:, -1] == c).sum()  # detections per class
-                s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
-
-            # Write results
-            for *xyxy, conf, cls in reversed(det):
-                if names[int(cls)] != "Human":
+            # Process detections
+            for i, det in enumerate(pred):  # detections per image
+                p, s, im0 = path, '', im0s
+                if len(det) == 0:
                     continue
-                label = f'{names[int(cls)]} {conf:.2f}'
-                plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
 
-                # ---UPLOADING TO FIREBASE REALTIME DB --MOST IMPORTANT THING TO WORK
-                if opt.connect_rtdb:
-                    if realtime.is_img_upload_finish() and float(conf) > 0.7:
-                        th1 = threading.Thread(target=realtime.add_image, args=(im0,))
-                        ts_img_data.append(th1)
-                        th1.start()
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    if names[int(cls)] != "Human":
+                        continue
+                    label = f'{names[int(cls)]} {conf:.2f}'
+                    plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
 
-                    if realtime.is_interference_upload_finish():
-                        th2 = threading.Thread(target=realtime.save_interference, args=(float(conf),))
-                        ts_logger.append(th2)
-                        th2.start()
-                    if opt.alarm and float(conf) >= 0.5:
-                        alarm_check = realtime.is_alarm_on()
-                        if alarm_check:
-                            if alarm.status == PlayerStatus.PLAYING:
-                                continue
-                            alarm.play()
-                        else:
-                            alarm.stop()
-                # --ENDED
-            print(f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}s) NMS')
+                    # ---UPLOADING TO FIREBASE REALTIME DB --MOST IMPORTANT THING TO WORK
+                    if opt.connect_rtdb:
+                        if realtime.is_img_upload_finish() and float(conf) > 0.7:
+                            th1 = threading.Thread(target=realtime.add_image, args=(im0,))
+                            ts_img_data.append(th1)
+                            th1.start()
 
-    print(f'Done. ({time.time() - t0:.3f}s)')
+                        if realtime.is_interference_upload_finish():
+                            th2 = threading.Thread(target=realtime.save_interference, args=(float(conf),))
+                            ts_logger.append(th2)
+                            th2.start()
+                        if opt.alarm and float(conf) >= 0.5:
+                            alarm_check = realtime.is_alarm_on()
+                            if alarm_check:
+                                if alarm.status == PlayerStatus.PLAYING:
+                                    continue
+                                alarm.play()
+                            else:
+                                alarm.stop()
+                    # --ENDED
+                print(f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}s) NMS')
+
+    except KeyboardInterrupt:
+        dataset.cap.release()
+        cv2.destroyAllWindows()
+
 
 
 if __name__ == '__main__':
@@ -145,3 +148,5 @@ if __name__ == '__main__':
         t.join()
     for t in ts_logger:
         t.join()
+    sys.exit(0)
+
